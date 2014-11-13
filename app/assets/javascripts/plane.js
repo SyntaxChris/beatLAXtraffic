733fde947358268f9c379057dc88b85d8c51481a
Chris Lehneis <cdlehneis@gmail.com>
2014-11-12 20:46:36 -0500

    uses jquery for plane to land. Just need to add class 'land-ze-plane' to element with id 'landing-plane'

--- app/assets/javascripts/plane.js
+++ app/assets/javascripts/plane.js
@@ -1,8 +1,5 @@
-// $(document).ready(function(){
-// 	$('#landing-plane').delay(1000).animate({  textIndent: -10 }, {
-//     	step: function(now,fx) {
-//      	 $(this).css('-webkit-transform','rotate('+now+'deg)'); 
-//     	},
-//     	duration:'slow'
-// 	},'linear');
-// });
+$(document).ready(function(){
+	$('#land').click(function(){
+		$('#landing-plane').addClass("land-ze-plane");
+	});
+});
