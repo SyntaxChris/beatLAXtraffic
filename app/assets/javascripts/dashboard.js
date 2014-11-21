c6c07ba0d235149bd9a110374c6175fcc4f79139
Chris Lehneis <cdlehneis@gmail.com>
2014-11-21 13:57:54 -0500

    clock added

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,20 +1,19 @@
 $(document).ready(function(){
-    var planeProgress = $('#plane-progress').data('plane-progress')-5;
+    var planeProgress = $('#plane-progress').data('plane-progress')-15;
        
-    $("#plane-bar-landscape").animate({width: planeProgress+"%"}, 15000);
-    $("#plane-bar-filler-landscape").animate({marginLeft: planeProgress+"%"}, 15000);
     $("#plane-bar").animate({width: planeProgress+"%"}, 15000);
-    $("#plane-bar-filler").animate({marginLeft: planeProgress+"%"}, 15000);
+    $("#filler").animate({marginLeft: planeProgress+"%"}, 16000);
     
     
     $(function () {
-        var prevWidth = $('#plane-bar-landscape').width();
-        $('#plane-bar-landscape').attrchange({
+        var prevWidth = $('#plane-bar').width();
+        $('#plane-bar').attrchange({
             callback: function (e) {
                 var curWidth = $(this).width();
-                var progressPercentage = curWidth/$('#bar-back-landscape').width()*100;            
+                var progressPercentage = curWidth/$('#bar-back').width()*100;            
                 if (prevWidth !== curWidth) {
-                    if (progressPercentage > 94) {     
+                    $("#logger").text(progressPercentage);
+                    if (progressPercentage > 84) {     
                         $("#timer-alert").attr("class", "timer-alert");
                         $("#plane-alert").attr("class", "timer-alert");
                         $('#landing-plane').addClass("land-ze-plane");
