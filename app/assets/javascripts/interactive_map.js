f8011f7264559bce46d3d78701329a2bb86756af
Jason Sohl <jason@frozensoliddesigns.com>
2014-12-08 21:48:33 -0800

    removing svgs options, using static images, positioning select buttons, building select functionality

--- app/assets/javascripts/interactive_map.js
+++ app/assets/javascripts/interactive_map.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
-  
+  //set some vars
+  var $interactiveMap = $('#interactive-map');
+
+  //listeners
+  $interactiveMap.on('click', '.x-btn-select-path',selectPath);
+
+  function selectPath(e){
+    //prevent the default and propagation
+    e.preventDefault();
+    e.stopImmediatePropagation();
+
+    //set the path
+    var $btn = $(this),
+        path = $btn.data('path');
+
+    //check if this one is selected already
+    if ($btn.hasClass('selected')) { return; }
+
+    //deselect selected classes
+    $interactiveMap
+      .find('.x-btn-select-path.selected').toggleClass('selected', false).end()
+      .find('.map-path.selected').toggleClass('selected', false);
+
+    //add selected classes
+    $interactiveMap
+      .find('.x-btn-select-path[data-path="'+path+'"]').toggleClass('selected', true).end()
+      .find('.map-path.'+path).toggleClass('selected', true);
+  }
+
+
 });
