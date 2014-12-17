501324c3635e21b2423d92dfa252b7bba3c92915
vcavallo <vcavallo@gmail.com>
2014-12-17 11:26:51 -0500

    Re-passes node index spec and reorganizes into questions/dps

--- spec/requests/api/nodes_spec.rb
+++ spec/requests/api/nodes_spec.rb
@@ -37,10 +37,34 @@ describe "nodes API" do
       expect(json.first["question"]).to eq "what?"
       expect(json.first["answers"]).to be_a(Array)
       expect(json.first["answers"].first["answer"]).to eq "this"
-      expect(json.second["situation"]).to eq "where to?"
-      expect(json.second["decisions"]).to be_a(Array)
-      expect(json.second["decisions"].first["decision"]).to eq "this way!"
-      expect(json.second["decisions"].first["destination_node_id"]).to eq 3
+    end
+
+    it "all decision points have their situations and decisions" do
+      q1.question = create(:question, node_id: q1.id, question: "what?")
+      a1 = create(:answer, question_id: q1.question.id, answer: "this")
+      a2 = create(:answer, question_id: q1.question.id, answer: "that")
+
+      q2.question = create(:question, node_id: q2.id, question: "what again?")
+      a3 = create(:answer, question_id: q2.question.id, answer: "this again")
+      a4 = create(:answer, question_id: q2.question.id, answer: "that again")
+
+      q3.question = create(:question, node_id: q3.id, question: "what yet again?")
+      a5 = create(:answer, question_id: q3.question.id, answer: "this yet again")
+      a6 = create(:answer, question_id: q3.question.id, answer: "that yet again")
+
+      dp1.decision_point = create(:decision_point, node_id: dp1.id, situation: "where to?")
+      d1 = create(:decision, decision_point_id: dp1.decision_point.id, decision: "this way!", destination_node: q3)
+      d2 = create(:decision, decision_point_id: dp1.decision_point.id, decision: "that way!", destination_node: q2)
+
+      get '/api/nodes'
+
+      dp_index = nil
+      json.each_with_index { |j,i| dp_index = i if j['is_decision_point'] == true }
+
+      expect(json[dp_index]["situation"]).to eq "where to?"
+      expect(json[dp_index]["decisions"]).to be_a(Array)
+      expect(json[dp_index]["decisions"].first["decision"]).to eq "this way!"
+      expect(json[dp_index]["decisions"].first["destination_node_id"]).to eq q3.id
     end
   end
 
