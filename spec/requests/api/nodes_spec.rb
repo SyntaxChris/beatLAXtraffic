4c85493bccf7fc707caaedb7dd4dcba3bdb855f5
vcavallo <vcavallo@gmail.com>
2014-12-17 11:56:10 -0500

    Removes some comments in spec

--- spec/requests/api/nodes_spec.rb
+++ spec/requests/api/nodes_spec.rb
@@ -8,13 +8,6 @@ describe "nodes API" do
     let!(:q3) { create(:question_node) }
     let!(:dp1) { create(:decision_node) }
 
-    #it "gets all nodes" do
-    #  get '/api/nodes'
-
-    #  expect(response).to be_success
-    #  expect(json.length).to eq 4
-    #end
-
     it "all question nodes have their questions and answers" do
       q1.question = create(:question, node_id: q1.id, question: "what?")
       a1 = create(:answer, question_id: q1.question.id, answer: "this")
@@ -67,30 +60,4 @@ describe "nodes API" do
       expect(json[dp_index]["decisions"].first["destination_node_id"]).to eq q3.id
     end
   end
-
-  # describe "a single node's content" do
-  #     let!(:question_node) { create(:node, is_decision_point: false) }
-  #     let!(:question) { create(:question, node_id: question_node.id, question: 'what?')}
-  #     let!(:answer_1) { create(:answer, question_id: question.id, answer: "this")}
-  #     let!(:answer_2) { create(:answer, question_id: question.id, answer: "that")}
-  #     let!(:answer_3) { create(:answer, question_id: question.id, answer: "the other thing")}
-  #     let!(:decision_node) { create(:node, is_decision_point: true) }
-  #     let!(:decision_point) {
-  #       create(:decision_point, node_id: decision_node.id, situation: "where to?")
-  #     }
-  #     let!(:decision_1) {
-  #       create(:decision, decision_point_id: decision_point.id, decision: "this way")
-  #     }
-  #     let!(:decision_2) {
-  #       create(:decision, decision_point_id: decision_point.id, decision: "that way")
-  #     }
-
-  #   it "can get its node's question and answer content" do
-  #     get "/api/nodes/#{question_node.id}"
-
-  #     expect(response).to be_success
-  #     expect(json['is_decision_point']).to eq false
-  #     expect(json['question']).to eq 'what?'
-  #   end
-  # end
 end
