195f4c27405efb4b89780594426c5f9e78d35fdc
Chris Lehneis <cdlehneis@gmail.com>
2015-01-08 17:31:53 -0500

    clock now interactive with each click, using toggle for now

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,11 +1,112 @@
 $(document).ready(function(){
-    var planeProgress = $('#plane-progress').data('plane-progress')-15;
+    // clock animation
+    var nextColor = 0;
+
+    $("#toggle-questions").click(function(){
+        $('svg circle#timer-fill').css("opacity", 0);
+
+        switch(nextColor){
+            case 0:
+                $('svg circle#timer-fill').css("fill", '#2656BD');
+            break;
+
+            case 1:
+                $('svg circle#timer-fill').css("fill", '#2779C1');
+            break;
+
+            case 2:
+                $('svg circle#timer-fill').css("fill", '#2AB1D0');
+            break;
+
+            case 3:
+                $('svg circle#timer-fill').css("fill", '#32BD26');
+            break;
+
+            case 4:
+                $('svg circle#timer-fill').css("fill", '#A0D32A');
+            break;
+
+            case 5:
+                $('svg circle#timer-fill').css("fill", '#FFFF33');
+            break;
+
+            case 6:
+                $('svg circle#timer-fill').css("fill", '#FFD333');
+            break;
+
+            case 7:
+                $('svg circle#timer-fill').css("fill", '#FFA733');
+            break;
+
+            case 8:
+                $('svg circle#timer-fill').css("fill", '#FF8133');
+            break;
+
+            case 9:
+                $('svg circle#timer-fill').css("fill", '#FF3333');
+            break;
+
+            case 10:
+                $('svg circle#timer-fill').css("fill", '#EE4544');
+            break;
+        }
+
+        var r = $('#clock-hand-cntr').data('rot') + 45;
+
+        $('#clock-hand-cntr').css({
+          '-webkit-transform': 'rotate('+r+'deg)',
+             '-moz-transform': 'rotate('+r+'deg)',
+              '-ms-transform': 'rotate('+r+'deg)',
+               '-o-transform': 'rotate('+r+'deg)',
+                  'transform': 'rotate('+r+'deg)'
+        });
+
+        $('#clock-hand-cntr').data('rot', r);
+        
+        var o = $('#clock-face').data('opac') + 0.125;
+
+        if(nextColor === 11){
+            nextColor = 0;
+        };
+
+        if($('#clock-face').data('opac') > 0.8){
+            $('#clock-face').data('opac', 0);
+            $('svg circle#timer-fill').css("opacity", 0);
+            nextColor += 1
+        }
+        else{
+            $('svg circle#timer-fill').css("opacity", o);
+            $('#clock-face').data('opac', o);
+        } 
+    });
+    // end clock animation
+
+    var landTime = ["30", "1", "2"][Math.floor(Math.random() * 3)];
+    $('.number.landing').text(landTime);
+    
+    var planeProgress;
+    if(landTime === "2"){
+        planeProgress = 0;
+        $("#hr").text(landTime);
+        $("#hr-unit").text("hrs");
+    }
+    else if(landTime === "1"){
+        planeProgress = 15;
+        $("#hr").text(landTime);
+        $("#hr-unit").text("hr");
+    }
+    else{
+        planeProgress = 37;
+        $("#min").text(landTime);
+        $("#min-unit").text("min");
+    }
+
 
     $('body').on('startDashboardAnimation', startDashboardAnimation);
 
     function startDashboardAnimation(){ 
-        $("#plane-bar").animate({width: planeProgress+"%"}, 15000);
-        $("#filler").animate({marginLeft: planeProgress+"%"}, 16000);
+        $("#plane-bar").animate({width: planeProgress+"%"}, 2000);
+        $("#filler").animate({marginLeft: planeProgress+"%"}, 2000);
         
         $(function () {
             var prevWidth = $('#plane-bar').width();
@@ -16,7 +117,6 @@ $(document).ready(function(){
                     if (prevWidth !== curWidth) {
                         $("#logger").text(progressPercentage);
                         if (progressPercentage > 84) {     
-                            // $("#timer-alert").attr("class", "timer-alert");
                             $("#plane-alert").attr("class", "timer-alert");
                             $('#landing-plane').addClass("land-ze-plane");
                         }
