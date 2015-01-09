68e2e1337ce170a2aa88a150b5256c57745c598d
Chris Lehneis <cdlehneis@gmail.com>
2015-01-09 15:50:39 -0500

    modified distance question

--- app/assets/javascripts/distance_questions.js
+++ app/assets/javascripts/distance_questions.js
@@ -1,4 +1,78 @@
 $(document).ready(function(){
+
+	$("#row-1").hover(
+  		function () {
+    		$('#circle-1').addClass("activate-circle");
+  		},
+  		function () {
+    		$('#circle-1').removeClass("activate-circle");
+  		}
+	);
+
+	$("#row-1").click(function(){
+		$("#house").animate({
+			"bottom":"341px"
+		});
+	});
+
+	$("#row-2").hover(
+  		function () {
+    		$('#circle-2').addClass("activate-circle");
+  		},
+  		function () {
+    		$('#circle-2').removeClass("activate-circle");
+  		}
+	);
+	$("#row-2").click(function(){
+		$("#house").animate({
+			"bottom":"281px"
+		});
+	});
+
+	$("#row-3").hover(
+  		function () {
+    		$('#circle-3').addClass("activate-circle");
+  		},
+  		function () {
+    		$('#circle-3').removeClass("activate-circle");
+  		}
+	);
+	$("#row-3").click(function(){
+		$("#house").animate({
+			"bottom":"221px"
+		});
+	});
+
+	$("#row-4").hover(
+  		function () {
+    		$('#circle-4').addClass("activate-circle");
+  		},
+  		function () {
+    		$('#circle-4').removeClass("activate-circle");
+  		}
+	);
+	$("#row-4").click(function(){
+		$("#house").animate({
+			"bottom":"161px"
+		});
+	});
+
+	$("#row-5").hover(
+  		function () {
+    		$('#circle-5').addClass("activate-circle");
+  		},
+  		function () {
+    		$('#circle-5').removeClass("activate-circle");
+  		}
+	);
+	$("#row-5").click(function(){
+		$("#house").animate({
+			"bottom":"101px"
+		});
+	});
+
+
+
 	$("#circle-1").click(function(){
 		$("#house").animate({
 			"bottom":"341px"
