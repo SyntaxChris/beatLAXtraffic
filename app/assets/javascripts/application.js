210d4c3f69dd75a311df82448056e37d60ef3fb6
vcavallo <vcavallo@gmail.com>
2015-01-13 14:48:48 -0500

    Updates NodeCtl setup function to all rely on nodes GET callback

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//
 //= require jquery
 //= require jquery_ujs
 //= require 'attribute.js'
@@ -43,4 +44,3 @@
 
 //= require_tree .
 //
-$(function(){ $(document).foundation(); });
