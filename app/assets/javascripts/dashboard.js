d1ddd8e3442bd96e02670d3b4c0107f4ca18f70d
Chris Lehneis <cdlehneis@gmail.com>
2015-01-12 21:15:45 -0500

    dashboard counter working properly

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,131 +1,41 @@
 $(document).ready(function(){
-
+    // set variables
     var landTime = ["30", "1", "2"][Math.floor(Math.random() * 3)];
-    var counterNum = $('#counter-num').data('counter-number');
-    var planeProgress = 0;
-    var nextColor = 0;
     var hour = $('#time').data('hours');
     var min = $('#time').data('minutes');
 
-    debugger;
+    var movePlane = function(incrementNum){
+        var currentProgress = $('#plane-progress').data('plane-progress');
+        var totalProgress = incrementNum + currentProgress;
     
-
-    $('#min').text(min);
-    $("#min-unit").text("min");
-    $('.number.landing').text(landTime);
-
-    // set initial start values for plane progress bar 
-    if(landTime === "2"){
-        $('#plane-progress').data('plane-progress', planeProgress += 0);
-        $("#plane-bar").animate({width: planeProgress+"%"}, 2000);
-        $("#filler").animate({marginLeft: planeProgress+"%"}, 2000);
-    }
-    else if(landTime === "1"){
-        $('#plane-progress').data('plane-progress', planeProgress += 20);
-        $("#plane-bar").animate({width: planeProgress+"%"}, 2000);
-        $("#filler").animate({marginLeft: planeProgress+"%"}, 2000);
-    }
-    else{
-        $('#plane-progress').data('plane-progress', planeProgress += 35);
-        $("#plane-bar").animate({width: planeProgress+"%"}, 2000);
-        $("#filler").animate({marginLeft: planeProgress+"%"}, 2000);
-    }
-
-    // target elements that increase counter time
-    $(".sign, #x-btn-keep-looking, #x-btn-cirle-next, #x-btn-timer-more, .next#spin-parking-lose, .next#spin-pickup-lose, .next#timer-question, #x-btn-circle-next, #x-btn-timer-next, #x-btn-wait-next").click(function(){
-        var idClicked = $(this).attr('id'); 
-
-        if(idClick === 'timer-question'){
-            $.each($(".timer-tile"), function( index, value ) {
-                if($(value).attr('id') === 'tile-1' && $(value).css('opacity') === '0.5'){
-                    counterNum += 90;
-                }
-                else if($(value).attr('id') === 'tile-2' && $(value).css('opacity') === '0.5'){
-                    counterNum += 15;
-                }
-                else if($(value).attr('id') === 'tile-3' && $(value).css('opacity') === '0.5'){
-                    counterNum += 60;
-                }
-                else if($(value).attr('id') === 'tile-4' && $(value).css('opacity') === '0.5'){
-                    counterNum += 30;
-                }
-            });
+        if( totalProgress < 85){
+            $('#plane-progress').data('plane-progress', totalProgress);
+            $("#plane-bar").animate({width: totalProgress+"%"}, 200);
+            $("#filler").animate({marginLeft: totalProgress+"%"}, 200);
         }
-        else if(idClick === 'x-btn-circle-next'){
-            var loopNum = 5 * parseInt($(".circle-number").text());
-            counterNum += loopNum;
-        }
-        else if(idClick === 'x-btn-timer-next'){
-            counterNum += parseInt($("div#min.timer-number").text());
-        }
-        else if(idClick === 'x-btn-wait-next'){
-            counterNum += parseInt($("div#wait.timer-number").text());
-        }
-        else{
-            counterNum += 5;
-        }
-
-
-
-        // set new value to counter data
-        $('#counter-num').data('counter-number', $('#counter-num').data('counter-number') += counterNum );
-    });
-
-
-
-
-    $("#toggle-questions").click(function(){
-
-        // plane progress logic
-        if(planeProgress < 85){
-            // temporarily increment
-            $('#plane-progress').data('plane-progress', planeProgress += 5);
-
-            $('#plane-progress').data('plane-progress', planeProgress);
-            $("#plane-bar").animate({width: planeProgress+"%"}, 200);
-            $("#filler").animate({marginLeft: planeProgress+"%"}, 200);
-        }
-        else{
+        else if (totalProgress >= 85){
+            $("#plane-bar").animate({width: "85%"}, 200);
+            $("#filler").animate({marginLeft: "85%"}, 200);
             $("#plane-alert").attr("class", "clock-pulse");
             $('#landing-plane').addClass("land-ze-plane");
             $('svg circle#timer-fill').attr("class", "clock-pulse");
-            // when plane lands
+
             window.setTimeout(function () {
                 $("#landing-label").text('Plane landed');
                 $("span#flight-status").text('Arrived');
             }, 5000);
-        }
 
+            $('#plane-progress').data('plane-progress', 85);
+        } 
+    }
+    var moveClock = function(){
+        var clockState = $('#clock-hand-cntr').data('rot');
+        var r = $('#clock-hand-cntr').data('rot') + 45;
 
-
-        // timer logic
-        $('#time').data('minutes', min += 5);
-        // $('#time').data('hours');
-        
-        if(min === 60){
-            $('#time').data('hours', hour += 1);
-            $('#hr').text(hour);
-            if(hour > 1){$("#hr-unit").text("hrs")}
-            else{$("#hr-unit").text("hr")}
-            
-            min = 0;
-            $('#time').data('minutes', min);
-            $('#min').empty();
-            $('#min-unit').empty();
-        }
-        else{
-            $('#time').data('minutes', min);
-            $('#min').text(min);
-            $('#min-unit').text('min');
-        }
-        // end timer logic
-    
-        
-        // clock animation
         if($('#landing-label').text() !== "Plane landed"){
             $('svg circle#timer-fill').css("opacity", 0);
 
-            switch(nextColor){
+            switch(clockState){
                 case 0:
                     $('svg circle#timer-fill').css("fill", '#2779C1');
                 break;
@@ -157,14 +67,14 @@ $(document).ready(function(){
 
             var o = $('#clock-face').data('opac') + 0.125;
 
-            if(nextColor === 6){
-                nextColor = 0;
+            if(clockState === 6){
+                clockState = 0;
             };
 
             if($('#clock-face').data('opac') > 0.8){
                 $('#clock-face').data('opac', 0);
                 $('svg circle#timer-fill').css("opacity", 0);
-                nextColor += 1
+                clockState += 1
             }
             else{
                 $('svg circle#timer-fill').css("opacity", o);
@@ -175,9 +85,6 @@ $(document).ready(function(){
             $('svg circle#timer-fill').css("opacity", 1);
         }
 
-
-        var r = $('#clock-hand-cntr').data('rot') + 45;
-
         $('#clock-hand-cntr').css({
           '-webkit-transform': 'rotate('+r+'deg)',
              '-moz-transform': 'rotate('+r+'deg)',
@@ -187,8 +94,111 @@ $(document).ready(function(){
         });
 
         $('#clock-hand-cntr').data('rot', r);
+    }
+    var counterDisplay = function(num){
+        var currentMin = $('#counter-num').data('counter-number') + num;
+        var currentHour = Math.floor(currentMin/60);
+
+        // minute logic
+        $('span#min').text(Math.round(currentMin/60 % 1 * 60));
         
-        
-        // end clock animation
+        // hour logic
+        if(currentHour < 1){
+           $('#hr').empty();
+           $('#hr-unit').empty();
+        }
+        else{
+            $('#hr').text(currentHour);
+
+            if(currentHour === 1){
+                $('#hr-unit').text("hr");
+            }
+            else{
+                $('#hr-unit').text("hrs");
+            }
+        }
+
+        $('#counter-num').data('counter-number', currentMin)
+    }
+
+
+
+
+
+    // set initial text values
+    $('span#min').text(0);
+    $("span#min-unit").text("min");
+    $('.number.landing').text(landTime);
+
+    // set initial start values for plane progress bar 
+    if(landTime === "2"){
+        $('#plane-progress').data('plane-progress', 0);
+        $("#plane-bar").animate({width: "0%"}, 2000);
+        $("#filler").animate({marginLeft: "0%"}, 2000);
+    }
+    else if(landTime === "1"){
+        $('#plane-progress').data('plane-progress', 20);
+        $("#plane-bar").animate({width: "20%"}, 2000);
+        $("#filler").animate({marginLeft: "20%"}, 2000);
+    }
+    else{
+        $('#plane-progress').data('plane-progress', 35);
+        $("#plane-bar").animate({width: "35%"}, 2000);
+        $("#filler").animate({marginLeft: "35%"}, 2000);
+    }
+
+    // target elements that increase counter time
+    $(".sign, #x-btn-keep-looking, #x-btn-cirle-next, #x-btn-timer-more, #spin-parking-lose, .next#spin-pickup-lose, .next#timer-question, #x-btn-circle-next, #x-btn-timer-next, #x-btn-wait-next").click(function(){
+        var idClicked = $(this).attr('id'); 
+
+        if(idClicked === 'timer-question'){
+            $.each($(".timer-tile"), function( index, value ) {
+                if($(value).attr('id') === 'tile-1' && $(value).css('opacity') === '0.5'){
+                    movePlane(30);
+                    moveClock();
+                    counterDisplay(90);
+                }
+                else if($(value).attr('id') === 'tile-2' && $(value).css('opacity') === '0.5'){
+                    movePlane(5);
+                    moveClock();
+                    counterDisplay(15);
+                }
+                else if($(value).attr('id') === 'tile-3' && $(value).css('opacity') === '0.5'){
+                    movePlane(20);
+                    moveClock();
+                    counterDisplay(60);
+                }
+                else if($(value).attr('id') === 'tile-4' && $(value).css('opacity') === '0.5'){
+                    movePlane(10);
+                    moveClock();
+                    counterDisplay(30);
+                }
+            });
+        }
+        else if(idClicked === 'x-btn-circle-next'){
+            var loopMin = 5 * parseInt($(".circle-number").text());
+            movePlane(loopMin);
+            moveClock();
+            counterDisplay(loopMin);
+        }
+        else if(idClicked === 'x-btn-timer-next'){
+            var timerNum = parseInt($("divspan#min.timer-number").text());
+            movePlane(timerNum);
+            moveClock();
+            counterDisplay(timeNum);
+        }
+        else if(idClicked === 'x-btn-wait-next'){
+            var waitNum = parseInt($("#wait.timer-number").text());
+            movePlane(waitNum);
+            moveClock();
+            counterDisplay(waitNum);
+        }
+        else{
+            movePlane(5);
+            moveClock();
+            counterDisplay(5);
+        }
     });
+
+
 });
