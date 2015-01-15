db4bb74380011c1ecbbc2707d76a5fd28a2687c9
vcavallo <vcavallo@gmail.com>
2015-01-15 12:08:14 -0500

    Removes duplicate respondent API call in AppCtrl

--- app/assets/javascripts/angular/controllers/AppCtrl.js
+++ app/assets/javascripts/angular/controllers/AppCtrl.js
@@ -1,15 +1,5 @@
 angular.module('lawaApp')
   .controller("AppCtrl", [
     '$scope','$http', function($scope,$http) {
-
-      $scope.currentSessionId = 'something';
-
-      $http({
-        url: '/api/respondents/get_or_create',
-        method: 'GET',
-        params: { session_id: $scope.currentSessionId }
-        }).success(function(data){
-          $scope.respondent = data;
-      });
     }
   ])
