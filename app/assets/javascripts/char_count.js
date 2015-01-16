f4be33da81facf388bd40abbd6cfc5cc81dcd17e
Chris Lehneis <cdlehneis@gmail.com>
2015-01-16 02:08:49 -0500

    working on js

--- app/assets/javascripts/char_count.js
+++ app/assets/javascripts/char_count.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
 	$("#char-count-large").text("140 characters left");
-	$('#other-info-large').keyup(function() {
-    	var franko = 140 - $(this).val().length;
-    	$("#char-count-large").text(franko + " characters left");
-	});
-	
-
 	$("[id='char-count']").text("140 characters left");
 	$("[id='char-count-small']").text("140 characters left");
 	$('[id=char-count-strategy]').text("140 characters left");
 	$("[id='char-count-last-questions']").text("140 characters left");
+
+	$(".char-count").text("140 characters left");
+
+	$('#tile-text').keyup(function() {
+	    var cs = 140 - $(this).val().length;
+	    $('.char-count').text(cs + " characters left");
+	});
+
+	$('[id=other-info-small]').keyup(function() {
+	    var cs = 140 - $(this).val().length;
+	    $('[id=char-count-small]').text(cs + " characters left");
+	});
 
 	
 	$('[id=other-info-small]').keyup(function() {
