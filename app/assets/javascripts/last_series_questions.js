9736044ef1d323602eb6931a10d4bccc93968713
Chris Lehneis <cdlehneis@gmail.com>
2015-01-19 18:34:14 -0500

    rendering weird

--- app/assets/javascripts/last_series_questions.js
+++ app/assets/javascripts/last_series_questions.js
@@ -2,43 +2,28 @@ $(document).ready(function(){
 	var otherBoxCount = 0;
   var qClicks = 0;
   var $body = $('body');
- 	
-  	$body.on('click',"#other-last", function(){
-  		if(otherBoxCount === 0){
-  			$("#last-questions-other-container").show();
-  		}
-      else if (otherBoxCount === 1){
-			  $("#last-questions-other-container").hide();
-  		}
-  	}); 
 
-  	$body.on('click', "#last-other-cancel", function(){
-  		$("#last-questions-other-container").hide();
-  	});
-
-  	$body.on('click', "#last-other-save", function(){
-  		$("#last-questions-other-container").hide();
-  	});
-
-  	$body.on('click', ".question-wrapper-1", function(){
-  		$("#question-set-1").hide();
-  		$("#question-set-2").fadeIn();
-  	});
-
-  	$body.on('click', "#last-other-save", function(){
-  		$("#question-set-1").hide();
-  		$("#question-set-2").fadeIn();
-  	});
-
-  	$body.on('click', ".question-wrapper-2", function(){
-  		$("#question-set-2").hide();
-  		$("#question-set-3").fadeIn();
-  	});
-
-    $body.on('click', '.question-wrapper-3', function(){
-      $('.question-wrapper-3').removeClass('q3-clicked');
-      $(this).addClass('q3-clicked');
-      $('.next').fadeIn();
-    });
-
+  $body.on('click', '.question-wrapper-1', function(){
+    $("#question-set-1").hide();
+    $("#question-set-2").fadeIn();
+  })
+  .on('click',"#other-last", function(){
+    if(otherBoxCount === 0){
+      $("#last-questions-other-container").show();
+      otherBoxCount = 1;
+    }
+    else if (otherBoxCount === 1){
+      $("#last-questions-other-container").hide();
+      otherBoxCount = 0;
+    }
+  })
+  .on('click', '.question-wrapper-2', function(){
+    $("#question-set-2").hide();
+    $("#question-set-3").fadeIn();
+  })
+  .on('click', '.question-wrapper-3', function(){
+    $('.question-wrapper-3').removeClass('q3-clicked');
+    $(this).addClass('q3-clicked');
+    $('.next').fadeIn();
+  })
 });
