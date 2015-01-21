d4974c86ad1362314a743d9a19eec2cd464af9dd
Chris Lehneis <cdlehneis@gmail.com>
2015-01-21 18:10:10 -0500

    animations complete

--- app/assets/javascripts/intro.js
+++ app/assets/javascripts/intro.js
@@ -6,17 +6,18 @@ $(document).ready(function(){
     .on('click', '#phone-next-btn', function(){
       $('#stage-1').hide();
       $('#stage-1-header').hide();
-      $('#stage-2').delay(500).fadeIn(1000);
-      $('#stage-2-header').fadeIn();
+      $('#stage-2').show();
+      $('#stage-2-header').show();
     })
     .on('click', '#stage-2-btn', function(){
+      $('#intro-phone').hide();
       $('#stage-2').hide();
       $('#stage-2-header').hide();
       $('#stage-3').delay(400).fadeIn();
       $("#speeding-car").animate({left: "110%"}, 5000);
       window.setTimeout(function () {
         $('#stage-3').fadeOut();
-        $('#stage-4, #stage-4-header').show();
+        $('#stage-4, #stage-4-header, #intro-phone').show();
       }, 5500);
       
     })
