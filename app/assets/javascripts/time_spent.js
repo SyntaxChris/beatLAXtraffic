01e521b8fa5328cfbe9658a6a7381b0d8d660ebc
Chris Lehneis <cdlehneis@gmail.com>
2015-01-28 11:48:14 -0500

    added random text to time spent template

--- app/assets/javascripts/time_spent.js
+++ app/assets/javascripts/time_spent.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
+	var outcomeAry = ["Nope, no parking", "This lot's all full", "Completely packed", "None here either", "No spaces", "Can't fit there"];
 	var timeSpent = 5,
-      $body = $('body');
+    $body = $('body');
+
+    $('.parking-result').text(outcomeAry[Math.floor(Math.random() * 7)])
 
 	$body.on('click', '#x-btn-keep-looking', function(){
 		timeSpent += 5;
 		$('.timer-c .inner .timer-number').text(timeSpent);
+		$('.parking-result').text(outcomeAry[Math.floor(Math.random() * 7)]);
 	});
 });
