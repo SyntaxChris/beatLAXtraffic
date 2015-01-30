a13a3a109223f7490491d1a9c0363773ce5feeb7
Chris Lehneis <cdlehneis@gmail.com>
2015-01-30 13:47:25 -0500

    revamped tile questions

--- app/assets/javascripts/tile_questions.js
+++ app/assets/javascripts/tile_questions.js
@@ -1,139 +1,79 @@
 $(document).ready(function(){
+	var phone = 0,
+		shop = 0,
+		hangout = 0,
+		eat = 0,
+		body = $('body');
 
-	var $body = $('body');
-	var phoneTog = 0;
-	var shopTog = 0;
-	var hangTog = 0;
-	var eatTog = 0;
-	var otherTog = 0;
-
-	$body
-	.on('click', '.tile-container', function(){
-		var tileId = $(".ng-scope div", this).attr('id');
-
-		switch(tileId){
-			case 'phone-landscape':
-				if(phoneTog === 0){
-
-					$('.check-mark-container svg .circle-state', this).css('stroke-width', '0');
-					$('.check-mark-container svg .circle-state', this).css('fill', '#FC9C52');
-					$('.check-mark-container svg .checkmark-fill', this).css('stroke', 'white');
-
-					phoneTog = 1;
-				}
-				else{
-
-					$('.check-mark-container svg .circle-state', this).css('stroke-width', '3');
-					$('.check-mark-container svg .circle-state', this).css('fill', 'white');
-					$('.check-mark-container svg .checkmark-fill', this).css('stroke', 'gray');
-					phoneTog = 0;
+	// var checkMark =
+	$body.on('click', '.tile svg', function(){
+		var tile = $(this).parents().eq(0).attr('id');
+		var checkMark = $(this).parents().eq(2).find('.tile-checkmark svg');
+	
+		switch(tile){
+			case 'phone':
+				if(phone === 0){
+					checkMark.find('path').css('fill', '#EF8E4B');
+					checkMark.find('circle').css('stroke-width', 0);
+					checkMark.find('polyline').css('stroke', '#FFFFFF');
+					phone = 1;
+				}else{
+					checkMark.find('path').css('fill', '#FFFFFF');
+					checkMark.find('circle').css('stroke-width', 3);
+					checkMark.find('polyline').css('stroke', '#757575');
+					phone = 0;
 				}
 			break;
 
-			case 'shop-landscape':
-			if(shopTog === 0){
-				$('.check-mark-container svg .circle-state', this).css('stroke-width', '0');
-				$('.check-mark-container svg .circle-state', this).css('fill', '#F7C001');
-				$('.check-mark-container svg .checkmark-fill', this).css('stroke', 'white');
-
-				shopTog = 1;
-			}
-			else{
-				$('.check-mark-container svg .circle-state', this).css('stroke-width', '3');
-				$('.check-mark-container svg .circle-state', this).css('fill', 'white');
-				$('.check-mark-container svg .checkmark-fill', this).css('stroke', 'gray');
-
-				shopTog = 0;
-			}
+			case 'shop':
+				if(shop === 0){
+					checkMark.find('path').css('fill', '#F2A700');
+					checkMark.find('circle').css('stroke-width', 0);
+					checkMark.find('polyline').css('stroke', '#FFFFFF');
+					shop = 1;
+				}else{
+					checkMark.find('path').css('fill', '#FFFFFF');
+					checkMark.find('circle').css('stroke-width', 3);
+					checkMark.find('polyline').css('stroke', '#757575');
+					shop = 0;
+				}
 			break;
 
-			case 'hang-out-landscape':
-			if(hangTog === 0){
-				$('.check-mark-container svg .circle-state', this).css('stroke-width', '0');
-				$('.check-mark-container svg .circle-state', this).css('fill', '#39BAD8');
-				$('.check-mark-container svg .checkmark-fill', this).css('stroke', 'white');
-
-				hangTog = 1;
-			}
-			else{
-				$('.check-mark-container svg .circle-state', this).css('stroke-width', '3');
-				$('.check-mark-container svg .circle-state', this).css('fill', 'white');
-				$('.check-mark-container svg .checkmark-fill', this).css('stroke', 'gray');
-
-				hangTog = 0;
-			}
+			case 'hang-out':
+				if(hangout === 0){
+					checkMark.find('path').css('fill', '#3AB1D6');
+					checkMark.find('circle').css('stroke-width', 0);
+					checkMark.find('polyline').css('stroke', '#FFFFFF');
+					hangout = 1;
+				}else{
+					checkMark.find('path').css('fill', '#FFFFFF');
+					checkMark.find('circle').css('stroke-width', 3);
+					checkMark.find('polyline').css('stroke', '#757575');
+					hangout = 0;
+				}
 			break;
 
-			case 'eat-landscape':
-			if(eatTog === 0){
-				$('.check-mark-container svg .circle-state', this).css('stroke-width', '0');
-				$('.check-mark-container svg .circle-state', this).css('fill', '#558AD8');
-				$('.check-mark-container svg .checkmark-fill', this).css('stroke', 'white');
-
-				eatTog = 1;
-			}
-			else{
-				$('.check-mark-container svg .circle-state', this).css('stroke-width', '3');
-				$('.check-mark-container svg .circle-state', this).css('fill', 'white');
-				$('.check-mark-container svg .checkmark-fill', this).css('stroke', 'gray');
-				eatTog = 0;
-			}
-			break;
-
-			case 'other-landscape':
-				$('.other-txt-bbl').fadeIn();
-				$('.other-txt-bbl-sm').fadeIn();
+			case 'eat':
+				if(eat === 0){
+					checkMark.find('path').css('fill', '#4983C9');
+					checkMark.find('circle').css('stroke-width', 0);
+					checkMark.find('polyline').css('stroke', '#FFFFFF');
+					eat = 1;
+				}else{
+					checkMark.find('path').css('fill', '#FFFFFF');
+					checkMark.find('circle').css('stroke-width', 3);
+					checkMark.find('polyline').css('stroke', '#757575');
+					eat = 0;
+				}
 			break;
 		}
 
-        var tallyTot = phoneTog + shopTog + hangTog + eatTog + otherTog;
-
-		if(tallyTot > 0){
-			$(".tile-next").fadeIn(500);
-		}
-		else{
-			$(".tile-next").fadeOut(300);
+		if(phone + shop + hangout + eat > 0){
+			$('.tile-next').fadeIn();
+		}else{
+			$('.tile-next').fadeOut();
 		}
 		
-	})
-	.on('click', '.cancel', function(){
-		$('.other-txt-bbl').fadeOut();
-		$('.other-txt-bbl-sm').fadeOut();
-		$('.tile-container.tile-5 .check-mark-container svg .circle-state').css('stroke-width', '3');
-		$('.tile-container.tile-5 .check-mark-container svg .circle-state').css('fill', 'white');
-		$('.tile-container.tile-5 .check-mark-container svg .checkmark-fill').css('stroke', 'gray');
-		$('textarea#tile-text').val("");
+	});
 
-		otherTog = 0;
-
-		var tallyTot = phoneTog + shopTog + hangTog + eatTog + otherTog;
-
-		if(tallyTot > 0){
-			$(".tile-next").fadeIn(500);
-		}
-		else{
-			$(".tile-next").fadeOut(300);
-		}
-	})
-	.on('click', '.bbl-btn.cancel', function(){
-		$('textarea.purple.small').val("");
-	})
-	.on('click', '.save', function(){
-		if($("textarea.purple").val() !== "" || $("textarea.purple.small").val() !== ""){
-			$('.other-txt-bbl').fadeOut();
-			$('.other-txt-bbl-sm').fadeOut();
-			$('.tile-container.tile-5 .check-mark-container svg .circle-state').css('stroke-width', '0');
-			$('.tile-container.tile-5 .check-mark-container svg .circle-state').css('fill', '#AA75D3');
-			$('.tile-container.tile-5 .check-mark-container svg .checkmark-fill').css('stroke', 'white');
-			otherTog = 1;
-		}
-
-		
-
-		var tallyTot = phoneTog + shopTog + hangTog + eatTog + otherTog;
-
-		if(tallyTot > 0){
-			$(".tile-next").fadeIn(500);
-		}
-	})
 });
