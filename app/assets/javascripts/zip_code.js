24be62c27d1b6d22fecb0721cbce0dd5d2c7114e
Chris Lehneis <cdlehneis@gmail.com>
2015-02-06 16:43:18 -0500

    just kidding, now the zipcode template bugs are fixed

--- app/assets/javascripts/zip_code.js
+++ app/assets/javascripts/zip_code.js
@@ -2,32 +2,28 @@ $(document).ready(function(){
 	var $body = $('body');
 	
 	$body
-	.on('keypress', "#zip-input", function(e){
-		var a = [];
-	    var k = e.which;
+		.on('keypress', "#zip-input", function(e){
+			if(e.which >= 48 && e.which <= 57){
+				var l = $(this).val().length;
 
-	    for (i = 48; i < 58; i++){
-	        a.push(i);
-	    }
+				if(l >= 4){
+					$('.zip-next').fadeIn();
+				}
+				else{
+					$('.zip-next').fadeOut();
+				}
+			}
+			else{
+				e.preventDefault();
+			}
+		})
+		.on('keydown', "#zip-input", function(){
+			var key = event.keyCode;
 
-	    if ((a.indexOf(k)>=0)){
-	    	var l = $(this).val().length;
-	    	if(l === 4){
-	    		$('.zip-next').fadeIn(200);
-	    	}
-	    }
-	    else{
-	    	e.preventDefault();
-	    }
-
-	})
-	.on('keydown', "#zip-input", function(){
-		var key = event.keyCode || event.charCode;
-
-		if(key == 8){
-			$('.zip-next').fadeOut(200);
-		}
-	});
+			if(key == 8){
+				$('.zip-next').fadeOut(200);
+			}
+		});
 });
 
 
