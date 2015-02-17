d1b63580da0f942228fc3710111707825932f3d3
Chris Lehneis <cdlehneis@gmail.com>
2015-02-16 19:20:59 -0500

    intro css fixes, timer_pickup fixes, car-dash css fixes, scalable last question text, adds favicon png

--- app/assets/javascripts/last_series_questions.js
+++ app/assets/javascripts/last_series_questions.js
@@ -3,17 +3,17 @@ $(document).ready(function(){
 
   $body
     .on('click',".question-wrapper-1-other", function(){
-      if($("#last-questions-other-container").css('display') === "none")
-        $("#last-questions-other-container").show()
+      if($(".free-form-popup").css('display') === "none")
+        $(".free-form-popup").show();
       else
-        $("#last-questions-other-container").hide();
-
+        $(".free-form-popup").hide();
     })
     .on('click', '.other-btn', function(){
-      if($(this).text() === "Cancel")
+      if($(this).text() === "Cancel"){
         $('textarea#other-info-last-questions').val('');
         $("[id='char-count-last-questions']").text("140 characters left");
-        $("#last-questions-other-container").hide();
+      }
+      $(".free-form-popup").hide();
     })
     .on('click', '.question-wrapper-3', function(){
       $('.question-wrapper-3').removeClass('q3-clicked');
