a45b61e31a7394200244921ac4454822067281ef
vinney cavallo <vcavallo@gmail.com>
2015-03-19 16:44:55 -0400

    Adds basic csv export

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -3,11 +3,20 @@ class ExportController < ApplicationController
   end
 
   def response_index
-    @responses = Response.all
+    @responses = Response.includes(
+      { :respondent => :unique_user },
+      :answer,
+      :decision,
+      { :node => :question }
+        #:question,
+        #:decision_point => :situation
+    )
     respond_to do |format|
-      format.html { render 'response_index' }
+      format.html do
+        render 'response_index'
+      end
       format.csv do
-        headers['Content-Disposition'] = "attachment; filename=\"response-list\""
+        headers['Content-Disposition'] = "attachment; filename=\"response-list.csv\""
         headers['Content-Type'] ||= 'text/csv'
       end
     end
