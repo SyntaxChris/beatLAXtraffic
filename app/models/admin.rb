3067afd401ec036a40cffb9e9cdb18055c2dbd77
vinney cavallo <vcavallo@gmail.com>
2015-03-20 14:19:11 -0400

    Starts to make admin-approveable accounts but stops

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,32 @@ class Admin < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   # :registerable
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+
+  #after_create :send_admin_mail
+
+  #def active_for_authentication?
+  #  super && approved?
+  #end
+
+  #def inactive_message
+  #  if !approved?
+  #    :not_approved
+  #  else
+  #    super :admin_approved
+  #  end
+  #end
+
+  #def self.send_reset_password_instructions(attributes={})
+  #  recoverable = find_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+  #  if !recoverable.approved?
+  #    recoverable.errors[:base] << I18n.t("devise.failure.not_approved")
+  #  elsif recoverable.persisted?
+  #    recoverable.send_reset_password_instructions
+  #  end
+  #  recoverable
+  #end
+
+  #def send_admin_mail
+  #  AdminMailer.new_user_waiting_for_approval(self).deliver
+  #end
 end
