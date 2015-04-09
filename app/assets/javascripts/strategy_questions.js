602530701fb94b010aa191182a6817d2a4250a17
Chris Lehneis <cdlehneis@gmail.com>
2015-04-09 14:56:04 -0400

    disables parking question for gq-1-b

--- app/assets/javascripts/strategy_questions.js
+++ app/assets/javascripts/strategy_questions.js
@@ -2,17 +2,23 @@ $(document).ready(function(){
 	var	$body = $('body');
 
 	$body.on('click', '.bbl', function(){
+		debugger;
 		if($(this).hasClass('other'))
 			$('.other-ctnr').show();
 		else
-			if($(this).hasClass('selected'))
-				$(this).removeClass('selected')
-			else
-				$(this).addClass('selected')
-			if($('.bbl').hasClass('selected'))
-				$('.next-ctnr').fadeIn()
-			else
-				$('.next-ctnr').fadeOut();
+			if($(this).hasClass('disabled')){
+				return;
+			}
+			else{
+				if($(this).hasClass('selected'))
+					$(this).removeClass('selected')
+				else
+					$(this).addClass('selected')
+				if($('.bbl').hasClass('selected'))
+					$('.next-ctnr').fadeIn()
+				else
+					$('.next-ctnr').fadeOut();	
+			}
 			
 	}).on('click', '.save, .cancel', function(){
 		if($(this).hasClass('save'))
