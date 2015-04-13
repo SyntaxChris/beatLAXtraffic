6189bcf1c8569cd6c7a0beed81015853c4245682
Chris Lehneis <cdlehneis@gmail.com>
2015-04-13 16:03:41 -0400

    removes animation for red car and removes button element

--- app/assets/javascripts/zip_code.js
+++ app/assets/javascripts/zip_code.js
@@ -1,33 +1,17 @@
 $(document).ready(function(){
 	var $body = $('body');
-
-	$(".zip-next").attr('disabled','disabled');
-
+	
 	$body.on('animateFerrari', animateFerrari);
 	
-	// $body
-	// 	.on('keypress', "#zip-input", function(e){
-	// 		if(e.which >= 48 && e.which <= 57){
-	// 			// var l = $(this).val().length;
-
-	// 			// if(l >= 4){
-	// 			// 	$('.zip-next').fadeIn();
-	// 			// }
-	// 			// else{
-	// 			// 	$('.zip-next').fadeOut();
-	// 			// }
-	// 		}
-	// 		else{
-	// 			e.preventDefault();
-	// 		}
-	// 	})
-	// 	.on('keydown', "#zip-input", function(){
-	// 		// var key = event.keyCode;
-
-	// 		// if(key == 8){
-	// 		// 	$('.zip-next').fadeOut(200);
-	// 		// }
-	// 	});
+	$body
+		.on('keypress', "#zip-input", function(e){
+			if(e.which >= 48 && e.which <= 57){
+				return;
+			}
+			else{
+				e.preventDefault();
+			}
+		})
 
 	$body.on('click', '.zip-next', function(){
 		if($(window).width() > 640){
